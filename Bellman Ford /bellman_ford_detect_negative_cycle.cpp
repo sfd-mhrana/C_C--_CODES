83c8b30e21bcb4513029072d66536bdd48ed23d2
sfd-mhrana <sfd.mhrana@gmail.com>
2024-02-24 11:37:22 +0600

    Bellman ford algorithm

---
+++ Bellman Ford /bellman_ford_detect_negative_cycle.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+class Edge
+{
+public:
+    int u, v, c;
+    Edge(int u, int v, int c)
+    {
+        this->u = u;
+        this->v = v;
+        this->c = c;
+    }
+};
+
+const int N = 1e5 + 5;
+int dis[N];
+
+int main()
+{
+
+    int n, e;
+    cin >> n >> e;
+    vector<Edge> edgeList;
+    while (e--)
+    {
+        int u, v, c;
+        cin >> u >> v >> c;
+        edgeList.push_back(Edge(u, v, c));
+        // IF undirected push another for revere one
+        //edgeList.push_back(Edge(u, v, c));
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
+    dis[0] = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (Edge edge : edgeList)
+        {
+            int u, v, c;
+            u = edge.u;
+            v = edge.v;
+            c = edge.c;
+            if (dis[u] < INT_MAX && dis[u] + c < dis[v])
+            {
+                dis[v] = dis[u] + c;
+            }
+        }
+    }
+
+    bool isCycle = false;
+    for (Edge edge : edgeList)
+    {
+        int u, v, c;
+        u = edge.u;
+        v = edge.v;
+        c = edge.c;
+        if (dis[u] < INT_MAX && dis[u] + c < dis[v])
+        {
+            isCycle = true;
+            break;
+        }
+    }
+
+    if (isCycle)
+    {
+        cout << "Cycle Detection" << endl;
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << dis[i] << endl;
+        }
+    }
+
+    return 0;
+};
--- CP_CONTENST_1926/c.cpp
+++ CP_CONTENST_1926/c.cpp
@@ -2,42 +2,31 @@
 using namespace std;
 #define ll long long
 
-ll getDigitSum(ll num)
-{
-    ll sum = 0;
-    while (num > 0)
-    {
-        ll last = num % 10;
-        sum += last;
-        num /= 10;
-    }
-    return sum;
-}
-void solve()
-{
-    ll num;
-    cin >> num;
-    ll sum = 0;
-    for (ll i = 1; i <= num; i++)
-    {
-        if (i > 9)
-        {
-            sum += getDigitSum(i);
-        }
-        else
-        {
-            sum += i;
-        }
-    }
-    cout << sum << endl;
-}
 int main()
 {
-    ll t;
+    const int N = int(2e5) + 10;
+    vector<int> v(N);
+
+    for (int i = 1; i < N; i++)
+    {
+        v[i] = v[i - 1];
+        int x = i;
+        while (x > 0)
+
+        {
+            v[i] += x % 10;
+            x = x / 10;
+        }
+    }
+
+    int t;
     cin >> t;
     while (t--)
     {
-        solve();
+
+        int n;
+        cin >> n;
+        cout << v[n] << endl;
     }
 
     return 0;
